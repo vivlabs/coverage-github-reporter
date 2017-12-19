84051aa35749ce273ca13228825038a02f6a89b8
Marcello Bastéa-Forte <marcello@viv.ai>
2017-12-19 09:20:40 -0800

    npm auth

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,6 +6,8 @@
 # only publish if this is a tag build
 if [[ "$CIRCLE_TAG" ]]; then
   set -e
+  # "login"
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
   # publish
   npm publish
 else
